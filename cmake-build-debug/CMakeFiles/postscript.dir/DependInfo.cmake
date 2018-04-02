f9a7a9016839ec61619bd3c3d483f2dc848e5fa1
Yongqi Yan <yanyongqi1991@qq.com>
2018-04-02 17:09:43 -0400

    Branch2
    Cmake and test.ps

---
+++ cmake-build-debug/CMakeFiles/postscript.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yan/postscript/main.cpp" "/Users/yan/postscript/cmake-build-debug/CMakeFiles/postscript.dir/main.cpp.o"
+  "/Users/yan/postscript/postscript.cpp" "/Users/yan/postscript/cmake-build-debug/CMakeFiles/postscript.dir/postscript.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/postscript.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/postscript.dir/main.cpp.o"
+  "CMakeFiles/postscript.dir/postscript.cpp.o"
+  "postscript.pdb"
+  "postscript"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/postscript.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
